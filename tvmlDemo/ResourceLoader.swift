94f1da104fbbef42b045be4d7f6fb14be1ef5326
Martin Reinhardt <contact@martinreinhardt-online.de>
2016-12-18 20:47:40 +0100

    Demo App

---
+++ tvmlDemo/ResourceLoader.swift
@@ -0,0 +1,50 @@
+//
+//  ResourceLoader.swift
+//  tvmlDemo
+//
+//
+
+import Foundation
+import JavaScriptCore
+
+
+@objc protocol ResourceLoaderExport : JSExport {
+  func getBasePath() -> String
+  func getPath(_ name: String, _ directory: String! ) -> String
+  func loadBundleResource(_ name: String, _ directory: String! ) -> String
+  static func create() -> ResourceLoaderExport
+}
+
+
+@objc class ResourceLoader: NSObject, ResourceLoaderExport {
+
+  func getBasePath( ) -> String {
+    return  Bundle.main.resourcePath!;
+  }
+
+  func getPath(_ name: String, _ directory: String! ) -> String {
+    let path :String?
+    if directory != nil {
+      path = Bundle.main.path(forResource: name, ofType: nil,inDirectory:directory)
+    } else {
+      path = Bundle.main.path(forResource: name, ofType: nil)
+    }
+    return path!
+  }
+
+  func loadBundleResource(_ name: String, _ directory: String!) -> String {
+    let path :String? = getPath(name, directory)
+    let fileContent: String
+    do {
+      fileContent = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+    } catch {
+      print("There was a problem")
+      return ""
+    }
+    return fileContent
+  }
+
+  static func create() -> ResourceLoaderExport {
+    return ResourceLoader()
+  }
+}
